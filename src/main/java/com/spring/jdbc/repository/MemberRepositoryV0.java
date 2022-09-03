ef2fca91bded23b28d8b70442756d4ded301f146
g1kwon <spring_shot@naver.com>
2022-09-03 16:19:43 +0900

    JDBC 개발 - insert

---
+++ src/main/java/com/spring/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,61 @@
+package com.spring.jdbc.repository;
+
+import com.spring.jdbc.connection.DBConnectionUtil;
+import com.spring.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManger 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            } }
+        if (stmt != null) {
+            try {
+                stmt.close(); //SQLEXCEPTION
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        } }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
---
+++ src/test/java/com/spring/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,18 @@
+package com.spring.jdbc.repository;
+
+import com.spring.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
+
+}
