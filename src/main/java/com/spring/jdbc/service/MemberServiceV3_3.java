7dc5a070faa1c4c464aa390089b7300548b8c71b
g1kwon <spring_shot@naver.com>
2022-09-09 10:27:15 +0900

    Spring Transaction AOP - @Transactional

---
+++ src/main/java/com/spring/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,41 @@
+package com.spring.jdbc.service;
+
+import com.spring.jdbc.domain.Member;
+import com.spring.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트래잭션 - Spring Transaction AOP @Transactional
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //비즈니스 로직 수행
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
