0013d3bac38ccc03fbdf84dd21a9ea112dc8af40
g1kwon <spring_shot@naver.com>
2022-09-09 16:28:41 +0900

    Exception - 예외 누수 문제 해결 with Interface

---
+++ src/main/java/com/spring/jdbc/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.spring.jdbc.repository;
+
+import com.spring.jdbc.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
---
+++ src/main/java/com/spring/jdbc/repository/MemberRepositoryEx.java
@@ -0,0 +1,12 @@
+package com.spring.jdbc.repository;
+
+import com.spring.jdbc.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepositoryEx {
+    Member save(Member member) throws SQLException;
+    Member findById(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
